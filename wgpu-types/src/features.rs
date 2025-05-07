2432a64f9ec55dd7c9140498ae2c53359e375028
Dmitry Zamkov <dmitry94@gmail.com>
2025-05-06 20:16:06 -0500

    Vulkan support for `SHADER_EARLY_DEPTH_TEST`

--- wgpu-types/src/features.rs
+++ wgpu-types/src/features.rs
@@ -1038,6 +1038,7 @@ bitflags_array! {
         /// Allows shaders to use the `early_depth_test` attribute.
         ///
         /// Supported platforms:
+        /// - Vulkan
         /// - GLES 3.1+
         ///
         /// This is a native only feature.
